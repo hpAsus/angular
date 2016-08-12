55fe16b47617a18ac1495e9c6f6befc59a1267ab
kaseopea <kaseopea@gmail.com>
2016-08-12 17:00:07 +0300

    updates

--- dist/dashboard/treeview/treeService.js
+++ dist/dashboard/treeview/treeService.js
@@ -1,26 +1,68 @@
-'use strict';
-
+"use strict";
 // Tree View Service
 // =====================================================================================================================
+
 (function () {
 
+    // NODE ENTITY
+    // =============================================================================
+    function NODE(title) {
+        this.id = '1';
+        this.metadata = {
+            'title': title
+        };
+        this._parent = null;
+        this._children = [];
+    }
+
+    // Set parent Method
+    // =============================================================================
+    Node.prototype.setParent = function (node) {
+        this._parent = node;
+    };
+
+    // Get parent Method
+    // =============================================================================
+    Node.prototype.getParent = function () {
+        return this._parent;
+    };
+
+    // Get children Method
+    // =============================================================================
+    Node.prototype.getChildren = function () {
+        var self = this;
+        return new Promise(function (resolve, reject) {
+            resolve(self._children);
+        });
+    };
+    // Add children Method
+    // =============================================================================
+    Node.prototype.addChildren = function (nodeArr) {
+        var self = this; //parent element
+
+        nodeArr.map(function (node) {
+            //setting guid
+            node.id = self.id + self._children.length;
+            // setting parent
+            node.setParent(self);
+            //adding child
+            self._children[self._children.length] = node;
+        });
+
+        return new Promise(function (resolve, reject) {
+            resolve(self._children); // no, we should export only added children i think
+        });
+    };
+
+    // Delete children Method
+    // =============================================================================
+    Node.prototype.removeChildren = function () {
+        this._children = [];
+    };
+
+    // =====================================================================================================================
+    // =====================================================================================================================
     var treeviewServiceFunc = function treeviewServiceFunc($http) {
-
-        // Tree
-        // =====================================================================
-
-        // NODE ENTITY
-        // =====================================================================
-        function NODE() {
-            var self = this;
-            self.id = randomIDGenerator();
-        }
-        NODE.prototype.getChildren = function () {};
-        NODE.prototype.addChildren = function (children) {};
-
-        // Helpers
-        // =====================================================================
-        function randomIDGenerator() {}
 
         // Trees
         // =====================================================================
