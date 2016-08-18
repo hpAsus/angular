6ededa87a79086df906c77c46d1827ec5c7923ab
kaseopea <kaseopea@gmail.com>
2016-08-18 19:08:24 +0300

    updates

--- app/actionButton/controllers/atActionItemCtrl.js
+++ app/actionButton/controllers/atActionItemCtrl.js
@@ -2,11 +2,15 @@
 // =====================================================================================================================
 (function () {
 
-    var atActionItemCtrlFunc = function (scope, element, attrs, $timeout) {
+    var atActionItemCtrlFunc = function (scope, element, attrs, $timeout, $interval) {
         var vm = this;
         var firstRun = true;
         var firstDelay = 3 * 1000;
+        var checkInterval = 5 * 1000;
+        var finalCheckDelay = 60 * 1000;
         var promiseCompleted = false;
+        var promiseError = false;
+        var checkIntervalID;
 
         //Statuses
         //0 - Play (Default)
@@ -16,34 +20,83 @@
         vm.statuses = ['play', 'progress', 'success', 'error'];
         vm.currentStatus = vm.statuses[0];
 
-        var promise = scope.ngModel;
 
         // Get Status
         function getStatus() {
             return vm.currentStatus;
         }
-        // Set status
 
+        // Set status
         function setStatus(status) {
             vm.currentStatus = vm.statuses[status];
         }
 
+        function isPromiseCompleted() {
+            if (promiseCompleted) {
+                promiseError ? setStatus(3) : setStatus(2);
+
+                //clear step
+                firstRun = false;
+                // if (checkIntervalID) {
+                //     console.log('interval cleared');
+                //     $interval.cancel(checkIntervalID);
+                // }
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+
         // Start Action
         vm.actionStart = function () {
+            var promise = scope.ngModel();
+
 
             if (firstRun) {
-                console.log('>>>>>>>>>>>>> First Run');
                 promise.then(function (res) {
                     promiseCompleted = true;
                     console.log('Promise arrived!');
-                    // setStatus(2);
+                }).catch(function (err) {
+                    promiseCompleted = true;
+                    promiseError = true;
                 });
 
                 setStatus(1);
 
+                //start delay
                 $timeout(function () {
-                    console.log('firscheck after first delay');
-                    setStatus(2);
+                    console.log('[Action started]');
+
+                    if (!isPromiseCompleted()) {
+
+                        console.log('[Delay passed] Action not completed yet');
+
+                        checkIntervalID = $interval(function () {
+                            if (isPromiseCompleted()) {
+                                console.log('[Interval cancelled]');
+                                $interval.cancel(checkIntervalID);
+                            } else {
+                                console.log('[Checking...] ', checkInterval);
+                            }
+                        }, checkInterval);
+
+                        // Final check
+                        $timeout(function () {
+                            console.log('[Final check]');
+                            if (checkIntervalID) {
+                                console.log('[Interval cancelled]');
+                                $interval.cancel(checkIntervalID);
+                            }
+                            if(!isPromiseCompleted()) {
+                                setStatus(3);
+                            };
+                        }, finalCheckDelay);
+
+                    } else {
+                        console.log('[Action completed]');
+                    }
+
                 }, firstDelay);
 
                 firstRun = false;
@@ -54,9 +107,8 @@
         }
 
 
-
     };
 
-    angular.module('atActionButton').controller('atActionItemCtrl', ['$scope', '$element', '$attrs', '$timeout', atActionItemCtrlFunc]);
+    angular.module('atActionButton').controller('atActionItemCtrl', ['$scope', '$element', '$attrs', '$timeout', '$interval', atActionItemCtrlFunc]);
 
 })();
--- app/pages/actiontest/actionTestCtrl.js
+++ app/pages/actiontest/actionTestCtrl.js
@@ -6,16 +6,37 @@
         var vm = this;
 
         // Test 1 Function that returns promise (fast no delay)
-        vm.testAction1 = $http({
-            method: 'GET',
-            url: '/action_req_1'
-        });
+        vm.testAction1 = function () {
+          return $http({
+              method: 'GET',
+              url: '/action_req_1'
+          });
+        };
 
         // Test 2 Slow
-        vm.testAction2 = $http({
-            method: 'GET',
-            url: '/action_req_2'
-        });
+        vm.testAction2 = function () {
+            return $http({
+                method: 'GET',
+                url: '/action_req_2'
+            });
+        };
+
+        // Test 3. 5 sec delay and error
+        vm.testAction3 = function () {
+            return $http({
+                method: 'GET',
+                url: '/action_req_3'
+            });
+        };
+
+
+        // Test 4. Slowest. More than final delay
+        vm.testAction4 = function () {
+            return $http({
+                method: 'GET',
+                url: '/action_req_4'
+            });
+        };
     
 
     };
